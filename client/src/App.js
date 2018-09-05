d5c75ed95ca9b0782695deb9e54f0cbac13a0fbc
Wesley Lewis <wesley.l.lewis@gmail.com>
2018-09-05 14:10:11 -0500

    Added Register Components, and Common Components

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,48 @@ import { setCurrentUser, logoutUser } from './actions/authActions';
 import { Provider } from 'react-redux';
 import store from './store';
 
-import NavBar from './components/layout/Navbar';
+import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Register from './components/auth/Register';
 
 import './App.css';
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Clear current profile
+    // store.dispatch(clearCurrentProfile());
+    // Redirect to login
+    store.window.location.href = '/login';
+  }
+}
+
 class App extends Component {
   render() {
-    return <h1>BETTY'S!</h1>;
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
+    );
   }
 }
 
--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -16,3 +16,45 @@ export const registerUser = (userData, history) => dispatch => {
       })
     );
 };
+
+// Login - Get User Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(res => {
+      // Save to localStorage
+      const { token } = res.data;
+      // Set token to localStorage
+      localStorage.setItem('jwtToken', token);
+      // Set token to Auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set Current User
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
+};
+
+// Logout Current User
+export const logoutUser = () => dispatch => {
+  // Remove token form localStorage
+  localStorage.removeItem('jwtToken');
+  // Remove Auth header for future requests
+  setAuthToken(false);
+  // Set current user to {} which will set isAuthenticated to false
+  dispatch(setCurrentUser({}));
+};
